25b90a9deecdc9e07f4a61975d79f2dda7889041
Nicole Harris <n.harris@kabucreative.com>
2015-10-12 15:59:49 +0100

    Create list_books view and template

--- books/views.py
+++ books/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def list_books(request):
-	return HttpResponse(request.user.username)
+	"""
+	List the books that have reviews
+	"""
+	
+	books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+	
+	context = {
+		'books': books,
+	}
+	
+	return render(request, "list.html", context)
