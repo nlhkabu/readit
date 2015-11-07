4f8c2969e8b3936aaab999b25d43ba91f335c726
Nicole Harris <n.harris@kabucreative.com>
2015-11-07 16:54:09 +0000

    Add custom form validation enforcing that each new book is unique

--- books/forms.py
+++ books/forms.py
@@ -27,3 +27,20 @@ class BookForm(forms.ModelForm):
 	class Meta:
 		model = Book
 		fields = ['title', 'authors']
+		
+	def clean(self):
+		# Super the clean method to maintain main validation and error messages
+		super(BookForm, self).clean()
+		
+		try:
+			title = self.cleaned_data.get('title')
+			authors = self.cleaned_data.get('authors')
+			book = Book.objects.get(title=title, authors=authors)
+			
+			raise forms.ValidationError(
+				'The book {} by {} already exists'.format(title, book.list_authors()),
+				code='bookexists'
+			)
+			
+		except Book.DoesNotExist:
+			return self.cleaned_data
