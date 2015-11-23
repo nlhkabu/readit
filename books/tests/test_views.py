2ad8a6da0020bcbcc5577d9f3c25fe92eb4b398f
Nicole Harris <n.harris@kabucreative.com>
2015-11-23 06:50:04 +0000

    Test ReviewList, add redirect for unauthenticated users

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
-from books.factories import AuthorFactory, BookFactory, ReviewFactory
-from books.views import list_books
+from books.factories import AuthorFactory, BookFactory, ReviewFactory, UserFactory
+from books.models import Book
+from books.views import list_books, ReviewList
 
 
 class TestListBooks(TestCase):
@@ -24,5 +25,50 @@ class TestListBooks(TestCase):
 		
 		self.assertEqual(books_with_reviews, books)
 		self.assertNotEqual(books_without_reviews, books)
+		
+
+class TestReviewList(TestCase):
+	def setUp(self):
+		self.user = UserFactory(username="test")
+		self.author = AuthorFactory()
+	
+	def test_reviews_url(self):
+		url = resolve('/review/')
+		self.assertEqual(url.func.__name__, ReviewList.__name__)
+	
+	def test_authentication_control(self):
+		# Check unauthenticated users cannot view page
+		response = self.client.get(reverse('review-books'))
+		self.assertEqual(response.status_code, 302)
+		
+		self.client.login(username="test", password="test")
+		response = self.client.get(reverse('review-books'))
+		self.assertEqual(response.status_code, 200)
+		
+		# While we're here, confirm we're using correct template
+		self.assertTemplateUsed(response, 'list-to-review.html')
+		
+	def test_review_list_returns_books_to_review(self):
+		# Setup our data
+		books_without_reviews = BookFactory.create_batch(2, authors=[self.author,])
+		
+		self.client.login(username="test", password="test")
+		response = self.client.get(reverse('review-books'))
+		
+		books = list(response.context['books'])
+		self.assertEqual(books, books_without_reviews)
+		
+	def test_can_create_new_book(self):
+		self.client.login(username="test", password="test")
+		response = self.client.post(
+			reverse('review-books'),
+			data={
+				'title': 'My Brand New Book',
+				'authors': [self.author.pk,],
+				'reviewed_by': self.user.pk,
+			},
+		)
+		
+		self.assertIsNotNone(Book.objects.get(title="My Brand New Book"))
 		
 	
